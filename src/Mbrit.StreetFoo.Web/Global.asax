b77fcc6aa072c76eb0b618036ab2417f088ff937
unknown <Matthew@AMXVMBR.(none)>
2012-02-09 16:30:32 +0000

    Fixed live database

---
+++ src/Mbrit.StreetFoo.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mbrit.StreetFoo.Web.Global" Language="C#" %>
--- src/Mbrit.StreetFoo/Objects/FooRuntime.cs
+++ src/Mbrit.StreetFoo/Objects/FooRuntime.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 
 namespace Mbrit.StreetFoo
 {
@@ -10,11 +11,27 @@ namespace Mbrit.StreetFoo
         private static string ConnectionUrl { get; set; }
         private static string DatabaseName { get; set; }
 
+        public static void Start()
+        {
+            string url = ConfigurationManager.AppSettings["MongoUrl"];
+            if (url == null)
+                throw new InvalidOperationException("'url' is null.");
+            if (url.Length == 0)
+                throw new InvalidOperationException("'url' is zero-length.");
+
+            // defer...
+            Start(url);
+        }
+
         public static void Start(string url)
         {
-            ConnectionUrl = url;
+            if (url == null)
+                throw new ArgumentNullException("url");
+            if (url.Length == 0)
+                throw new ArgumentException("'url' is zero-length.");
 
-            // get...
+            // go...
+            ConnectionUrl = url;
             DatabaseName = new Uri(url).LocalPath.Substring(1);
         }
 
